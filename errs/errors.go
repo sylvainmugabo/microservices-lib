a5c5967700c8fe8208356eda4aef92e3ee106e91
Nkurunziza Tabazi <sylvain.tabazi@gmail.com>
2023-09-10 08:32:16 -0400

    library project

---
+++ errs/errors.go
@@ -0,0 +1,49 @@
+package errs
+
+import "net/http"
+
+type AppError struct {
+	Code    int    `json:",omitempty"`
+	Message string `json:"message"`
+}
+
+func (app AppError) AsMessage() *AppError {
+	return &AppError{
+		Message: app.Message,
+	}
+}
+
+func NewNotFoundError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusNotFound,
+		Message: message,
+	}
+}
+
+func NewUnexpectedError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusInternalServerError,
+		Message: message,
+	}
+}
+
+func NewValidationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnprocessableEntity,
+		Message: message,
+	}
+}
+
+func NewAuthenticationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusBadRequest,
+		Message: message,
+	}
+}
+
+func NewAuthorizationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnauthorized,
+		Message: message,
+	}
+}
